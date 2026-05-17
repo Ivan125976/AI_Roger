6fe26dd5190c4bea5e490d935068a2f33472f988
Ivan125976@ <ivanprogrammist2345@mail.ru>
2026-05-17 14:54:01 +0300

    екстра фиксы

--- Yocto_Roger/Training.cs
+++ Yocto_Roger/Training.cs
@@ -28,15 +28,19 @@ namespace Yocto_Roger
                             oldOutputWeights[x, y] = outputWeights[x, y];
 
                     for (int x = 0; x < Parameters.Mlayers - 1; x++)
+                    {
+                        oldMiddleWeights[x] = new double[middleNeurons.GetLength(1),middleNeurons.GetLength(1)];
                         for (int y =0; y < middleWeights.GetLength(1); y++)
                             for (int z = 0; z < middleWeights.GetLength(1); z++)
                                 oldMiddleWeights[x][y, z] = middleWeights[x][y, z];
+
+                    }    
 
                     int[] binary = AIMath.NumToBin(educationArray[i, 0], inputNeurons.Length);
 
                     //forward propagation
                     NeuralNetwork.ForwardPropagation(binary, inputNeurons, inputWeights, middleNeurons, middleWeights, middleBiases, outputNeurons, outputBiases, outputWeights, NeuralNetwork.GenerateDropOut());
-
+                    double[] correctOutput = new double[educationArray.GetLength(1)];
                     for (int j = 0; j < outputNeurons.Length; j++)
                     {
                         errorOut[j] = outputNeurons[j] - binary[j]; //ошибка
@@ -47,7 +51,7 @@ namespace Yocto_Roger
 
                         outputBiases[j] -= deltaOut[j] * Parameters.learningRate;
                     }
-                    for (int j = 0; j < middleNeurons.Length; j++)
+                    for (int j = 0; j < middleNeurons.GetLength(1); j++)
                     {
                         for (int l = 0; l < outputNeurons.Length; l++)
                         {
@@ -60,16 +64,16 @@ namespace Yocto_Roger
                             deltaMid[Parameters.Mlayers - 1, j] = errorMid[Parameters.Mlayers - 1, j] * middleNeurons[Parameters.Mlayers - 1, j] * (1 - middleNeurons[Parameters.Mlayers - 1, j]); //дельта
 
                         for (int k = 0; k < outputNeurons.Length; k++)
-                            middleWeights[Parameters.Mlayers - 2][k, j] -= middleNeurons[Parameters.Mlayers - 1,k] * deltaMid[Parameters.Mlayers-1,j] * Parameters.learningRate;
+                            middleWeights[Parameters.Mlayers - 2][k, j] -= middleNeurons[Parameters.Mlayers - 2,k] * deltaMid[Parameters.Mlayers-1,j] * Parameters.learningRate;
 
                         middleBiases[Parameters.Mlayers - 1, j] -= deltaMid[Parameters.Mlayers - 1, j] * Parameters.learningRate;
                     }
                     for (int layer = Parameters.Mlayers - 2; layer >= 0; layer--)
                     {
                         int oldLayer = layer + 1;
-                        for (int j = 0; j < middleNeurons.Length; j++)
+                        for (int j = 0; j < middleNeurons.GetLength(1); j++)
                         {
-                            for (int l = 0; l < middleNeurons.Length; l++)
+                            for (int l = 0; l < middleNeurons.GetLength(1); l++)
                             {
                                 errorMid[layer, j] += deltaMid[oldLayer,l] * oldMiddleWeights[oldLayer][j, l]; //ошибка
                             }
@@ -80,7 +84,7 @@ namespace Yocto_Roger
                                 deltaMid[layer, j] = errorMid[layer, j] * middleNeurons[layer, j] * (1 - middleNeurons[layer, j]); //дельта
 
                             for (int k = 0; k < outputNeurons.Length; k++)
-                                middleWeights[Parameters.Mlayers - 2][k, j] -= middleNeurons[layer, k] * deltaMid[layer, j] * Parameters.learningRate;
+                                middleWeights[Parameters.Mlayers - 2][k, j] -= middleNeurons[oldLayer, k] * deltaMid[layer, j] * Parameters.learningRate;
 
                             middleBiases[layer, j] -= deltaMid[layer, j] * Parameters.learningRate;
                         }
